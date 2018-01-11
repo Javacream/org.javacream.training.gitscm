a9e0d4ae23d710d17cd59f4e29e0be8f7c599677
Rainer Sawitzki <training@rainer-sawitzki.de>
2018-01-11 12:24:32 +0100

    add interactive rebase example

---
+++ scripts/rebase_interactive_example.bat
@@ -0,0 +1,45 @@
+call C:\Users\Administrator\git\org.javacream.training.gitscm\scripts\create-file-repository.bat
+rmdir /s /q clone1
+rmdir /s /q clone2
+mkdir clone1
+cd clone1
+git clone ../server
+cd ..
+mkdir clone2
+cd clone2
+git clone ../server
+
+cd ../clone1/server
+git branch demofeature_1
+git checkout demofeature_1
+echo Hello > demofeature_1.txt
+git add demofeature_1.txt
+git commit -m "add demofeature_1.txt"
+echo Hello Again > demofeature_1.txt
+git commit -a -m "update demofeature_1.txt"
+echo Hello Again and again> demofeature_1.txt
+git commit -a -m "update demofeature_1.txt"
+git pull --rebase origin master
+git pull origin master
+git rebase --interactive master
+git checkout master
+git merge demofeature_1
+git push
+
+cd ../../clone2/server
+git branch demofeature_2
+git checkout demofeature_2
+echo Hello > demofeature_2.txt
+git add demofeature_2.txt
+git commit -m "add demofeature_2.txt"
+echo Hello Again > demofeature_2.txt
+git commit -a -m "update demofeature_2.txt"
+echo Hello Again and again> demofeature_2.txt
+git commit -a -m "update demofeature_2.txt"
+git pull --rebase origin master
+git pull origin master
+git rebase -i master
+git checkout master
+git merge demofeature_2
+git push
+
