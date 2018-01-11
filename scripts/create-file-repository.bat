374ec78f1540d479f539ca3047b393948db49f0f
Rainer Sawitzki <training@rainer-sawitzki.de>
2018-01-11 09:56:41 +0100

    add create filebased server repository

---
+++ scripts/create-file-repository.bat
@@ -0,0 +1,16 @@
+mkdir server
+cd server
+git init --bare
+cd ..
+mkdir temp
+cd temp
+git clone ../server
+cd server
+echo "Simple Git Server Project" > Readme.txt
+git add Readme.txt
+git commit -m "initialize project"
+git branch develop
+git push --all origin
+cd ..
+cd ..
+rmdir /s /q temp
