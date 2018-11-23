76cb05fd492c4fa45ea78b6f174cedd30155d2e9
Rainer Sawitzki <training@rainer-sawitzki.de>
2018-11-23 08:20:16 +0100

    add linux script for file-repository

---
+++ scripts/create-file-repository.sh
@@ -0,0 +1,18 @@
+rm -r -f server
+rm -r -f temp
+mkdir server
+cd server
+git init --bare
+cd ..
+mkdir temp
+cd temp
+git clone ../server
+cd server
+echo "Simple Git Server Project" > Readme.txt
+git add Readme.txt
+git commit -m "initialize project"
+git branch develop
+git push --all origin
+cd ..
+cd ..
+rm -r -f temp
