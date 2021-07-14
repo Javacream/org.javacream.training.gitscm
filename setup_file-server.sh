c6112ccf9c0b63c821d8b61e7b09af33b75733b0
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2021-07-14 08:47:27 +0200

    Add files via upload

--- setup_file-server.sh
+++ setup_file-server.sh
@@ -11,10 +11,7 @@ cd server
 echo "Simple Git Server Project" > Readme.txt
 git add Readme.txt
 git commit -m "initialize project"
-git branch develop
 git push --all origin
 cd ..
 cd ..
 rm -r -f temp
-
-
