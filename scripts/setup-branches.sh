3c9123c0613ba7a048e15bcaf1f1315531a2b7da
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2023-09-11 14:03:51 +0200

    Update setup-branches.sh

--- scripts/setup-branches.sh
+++ scripts/setup-branches.sh
@@ -1,31 +1,35 @@
 rm -rf training_branches
 mkdir training_branches
 cd training_branches
-git init
-echo "Branches Training" > README.txt
-git add README.txt
+git init --initial-branch=main
+echo Branches > README.txt
+git add .
 git commit -m "setup project"
-echo "Hello 1 master" > content.txt
-git add content.txt
-git commit -m "add content"
-echo "Hello 2 master" > content.txt
-git commit -a -m "change content"
-git branch feature1
-git branch feature2
-git checkout feature1
-echo "feature1" > content-feature1.txt
-git add content-feature1.txt
-git commit -a -m "add content-feature1"
-git checkout feature2
-echo "feature2" > content-feature2.txt
-git add content-feature2.txt
-git commit -a -m "add content-feature2"
-git branch feature2_part1
-git branch feature2_part2
-git checkout feature2_part1
-echo "feature2, part1" > content-feature2.txt
-git commit -a -m "change content-feature2, part1"
-git checkout feature2_part2
-echo "feature2, part2" > content-feature2.txt
-git commit -a -m "change content-feature2, part2"
-git checkout master
+git tag v1.0
+git branch feature/star
+git branch feature/planet
+git checkout feature/star
+echo Vega > star.txt
+git add .
+git commit -m "add vega"
+echo Sol > star.txt
+git add .
+git commit -m "change to Sol"
+git checkout feature/planet
+echo Venus > planet.txt
+git add .
+git commit -m "add venus"
+git branch feature/planet_part1
+git branch feature/planet_part2
+git checkout feature/planet_part1
+echo Mercury > planet.txt
+git add .
+git commit -m "add Mercury"
+git checkout feature/planet_part2
+echo Uranus > planet.txt
+git add .
+git commit -m "add Uranus"
+echo Jupiter > planet.txt
+git add .
+git commit -m "add Jupiter"
+git checkout main
