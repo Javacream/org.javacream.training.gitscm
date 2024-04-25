4b369cc526e508bd7aeb5720094a10278d78af8b
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-04-25 14:43:21 +0200

    Update setup-branches.sh

--- scripts/setup-branches.sh
+++ scripts/setup-branches.sh
@@ -11,25 +11,25 @@ git branch feature/planet
 git checkout feature/star
 echo Vega > star.txt
 git add .
-git commit -m "add vega"
+git commit -m " initialize vega"
 echo Sol > star.txt
 git add .
 git commit -m "change to Sol"
 git checkout feature/planet
 echo Venus > planet.txt
 git add .
-git commit -m "add venus"
+git commit -m "initialize  venus"
 git branch feature/planet_part1
 git branch feature/planet_part2
 git checkout feature/planet_part1
 echo Mercury > planet.txt
 git add .
-git commit -m "add Mercury"
+git commit -m "change  Mercury"
 git checkout feature/planet_part2
 echo Uranus > planet.txt
 git add .
-git commit -m "add Uranus"
+git commit -m "change Uranus"
 echo Jupiter > planet.txt
 git add .
-git commit -m "add Jupiter"
+git commit -m "change Jupiter"
 git checkout main
